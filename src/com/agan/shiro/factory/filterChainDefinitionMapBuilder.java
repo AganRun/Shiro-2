3792560d07f9f0e876eefe9c581257d6b5b5e941
AganRun <1379154067@qq.com>
2018-12-09 11:19:04 +0800

    22. 尚硅谷_Shiro_从数据表中初始化资源和权限

---
+++ src/com/agan/shiro/factory/filterChainDefinitionMapBuilder.java
@@ -0,0 +1,19 @@
+package com.agan.shiro.factory;
+
+import java.util.LinkedHashMap;
+
+public class filterChainDefinitionMapBuilder {
+
+	public LinkedHashMap<String, String> buildfilterChainDefinitionMap(){
+		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+		
+		map.put("/login.jsp", "anon");
+		map.put("/shiro/login", "anon");
+		map.put("/shiro/logout", "logout");
+		map.put("/admin.jsp", "roles[admin]");
+		map.put("/user.jsp", "roles[user]");
+		map.put("/**", "authc");
+		
+		return map;
+	}
+}
